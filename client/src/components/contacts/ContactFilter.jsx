f9ae072c0fc32c890fc4c9c05fa754408fb7d078
vm947 <vm947@nyu.edu>
2020-05-19 13:23:40 +0300

    changes

--- client/src/components/contacts/ContactFilter.jsx
+++ client/src/components/contacts/ContactFilter.jsx
@@ -1,27 +1,27 @@
 import React, { useContext, useRef, useEffect } from "react";
 import ContactContext from "../../context/contact/contactContext";
-import contactContext from './../../context/contact/contactContext';
+import contactContext from "./../../context/contact/contactContext";
 
 const ContactFilter = () => {
   const contactContext = useContext(ContactContext);
-  const text = useRef('');
+  const text = useRef("");
 
-  const {filterContacts, clearFilter, filtered} =  contactContext;
+  const { filterContacts, clearFilter, filtered } = contactContext;
 
-    useEffect(() =>{
-        if(filtered === null){
-            text.current.value = '';
-        }
-    });
-
-  const onChange = e =>{
-    if (text.current.value !== ''){
-        filterContacts(e.target.value);
-    } else {
-        clearFilter();
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = "";
     }
-  }
-  
+  });
+
+  const onChange = (e) => {
+    if (text.current.value !== "") {
+      filterContacts(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
+
   return (
     <form>
       <input
