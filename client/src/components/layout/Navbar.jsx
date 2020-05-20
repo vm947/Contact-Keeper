168744aef38f8f47329ce7c9d6d4b758899342d4
vm947 <vm947@nyu.edu>
2020-05-20 21:00:45 +0300

    changes

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Navbar = ({title, icon}) => {
-    return (
-        <div className="navbar bg-primary">
-            <h1>
-                <i className={icon}/> {title}
-            </h1>
-            <ul>
-                <li>
-                   <Link to="/">Home</Link>
-                </li>
-                <li>
-                   <Link to="/about">About</Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-}
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
 
 Navbar.defaultProps = {
-    title: 'Contact Keeper',
-    icon: 'fa fa-id-card-alt'
-}
+  title: "Contact Keeper",
+  icon: "fa fa-id-card-alt",
+};
